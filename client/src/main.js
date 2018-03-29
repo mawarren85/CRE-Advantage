481bd6ce416553068d3b1e9246bf79f33c6a3b72
mawarren85 <mawarren85@gmail.com>
2018-03-28 23:04:34 -0500

    changed import to const

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import VueSweetalert2 from 'vue-sweetalert2';
+// import Vue from 'vue'
+// import App from './App'
+// import router from './router'
+// import VueSweetalert2 from 'vue-sweetalert2';
+
+const Vue = require('vue')
+const App = require ('./App')
+const router = require('./router')
+// const VueSweetalert2 from 'vue-sweetalert2';
 
 
-import Nav from './components/Nav.vue'
+// const Nav = require(from './components/Nav.vue'
 Vue.component('Nav', Nav)
 
 Vue.config.productionTip = false
