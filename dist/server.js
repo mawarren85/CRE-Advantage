dfe60cc7a10d762f84ce6fa8fe7d50c13d927e75
mawarren85 <mawarren85@gmail.com>
2018-03-30 17:53:09 -0500

    workk

--- dist/server.js
+++ dist/server.js
@@ -2,13 +2,12 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static')
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')))
+// app.use(serveStatic(path.join(__dirname, 'dist')))
 
-console.log('HEEEEEEEEEEEEYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY');
 var port = process.env.PORT || 8080;
 app.listen(port);
 
-// app.get("/", function(req, res) {
-//   console.log(res, 'RESRESRESRESRESRSESRS');
-//   res.sendFile("index.html")
-// })
+app.get("/", function(req, res) {
+  console.log('HEEEEEEEEEEEEYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY');
+  res.render("index.html")
+})
