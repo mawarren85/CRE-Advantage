d81c1803ab0475c416cf1e57d0ca2fb61eb35a74
mawarren85 <mawarren85@gmail.com>
2018-03-30 17:49:32 -0500

    serve static npm

--- dist/server.js
+++ dist/server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var path = require('path');
-
+var serveStatic = require('serve-static')
 app = express();
-
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
 console.log('HEEEEEEEEEEEEYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY');
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port);
 
-app.get("/", function(req, res) {
-  console.log(res, 'RESRESRESRESRESRSESRS');
-  res.sendFile("index.html")
-})
+// app.get("/", function(req, res) {
+//   console.log(res, 'RESRESRESRESRESRSESRS');
+//   res.sendFile("index.html")
+// })
