06c651a5f145960000712fbc04b419ec0a4a855c
mawarren85 <mawarren85@gmail.com>
2018-03-30 17:20:11 -0500

    pray

--- dist/server.js
+++ dist/server.js
@@ -1,3 +1,12 @@
+var express = require('express');
+var path = require('path');
+
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
+
+
+
 app.get("/", function(req, res) {
   console.log(res, 'RESRESRESRESRESRSESRS');
   res.sendFile("index.html")
